9aeaffd7bf8f790d3bd83cf11e071240ecb64b34
Sharad Sonkar <sharadsonkarofficial@gmail.com>
2020-08-23 20:06:18 +0530

    add two pages

--- js/main.js
+++ js/main.js
@@ -36,3 +36,23 @@ $('#testimonial-carousel').owlCarousel({
     dots:true,
     navText: ["<img src='./images/icon-white-left.png'>","<img src='./images/icon-white-right.png'>"]
 })
+
+$('#categoryCarousel').owlCarousel({
+    loop:true,
+    autoplay:true,
+    margin:10,
+    nav:true,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:5
+        }
+    },
+    navText: ["<img src='./images/icon-prev-hook.png'>","<img src='./images/icon-next-hook.png'>"]
+});
